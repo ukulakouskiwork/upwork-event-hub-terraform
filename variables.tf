5023357fa45b0eca6ec2e4930eba41c323b667d0
Uladzislau Kulakouski <uladzislau.kulakouski@epam.com>
2022-12-11 14:36:30 +0400

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -1,53 +1,63 @@
 variable "rg_name" {
-  description = ""
+  description = "The name of the resource group where the eventhub should be created."
   type        = string
+  default = ""
 }
 
 variable "location" {
-  description = ""
+  description = "Region where your resources should be created."
   type        = string
+  default = "centralus" #for example
 }
 
 variable "storage_name" {
-  description = ""
+  description = "The name of the storage account"
   type        = string
+  default = "saeventhub"
 }
 
 variable "storage_container_list" {
-  description = ""
+  description = "List of containers that will be created in your storage account"
   type        = list(any)
+  default = []
 }
 
 
 variable "eventhub_namespace_name" {
-  description = "Name of the resource group to be imported."
+  description = "Name of the eventhub namespace."
   type        = string
+  default = "eh-namespace-test" # for example
 }
 
 
 variable "eventhub_namespace_sku" {
-  description = "Name of the resource group to be imported."
+  description = "Defines which tier to use. Valid options are Basic, Standard, and Premium"
   type        = string
+  default = "Standard"
 }
 
 
 variable "eventhub_namespace_capacity" {
-  description = "Name of the resource group to be imported."
-  type        = string
+  description = "pecifies the Capacity / Throughput Units for a Standard SKU namespace. Default capacity has a maximum of 2, but can be increased in blocks of 2 on a committed purchase basis."
+  type        = number
+  default = 2
 }
 
 variable "eventhub_name" {
-  description = "Name of the resource group to be imported."
+  description = "Name of the eventhub."
   type        = string
+  default = "ev-eventhub"
 }
 
 variable "eventhub_partition_count" {
-  description = "Name of the resource group to be imported."
+  description = "Specifies the current number of shards on the Event Hub."
   type        = number
+  default = 2
 }
 
 variable "eventhub_message_retention" {
-  description = "Name of the resource group to be imported."
+  description = "Specifies the number of days to retain the events for this Event Hub."
   type        = number
+  default = 1
 }
 
